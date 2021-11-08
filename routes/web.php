eaff6a2ce8f92e04c651012ed742cc663534270e
Kryztan Santos <kryztan.s@gmail.com>
2021-11-08 22:10:22 +0800

    Create Students index page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('students.index');
-});
+Route::get('/', [StudentController::class, 'index'])->name('home');
 
 Route::get('students', function () {
     return view('students.index');
